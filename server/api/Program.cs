ed3bbd9b2da410043db55a6442440b560c739c42
Zhovzan <zhovzan@yahoo.com>
2026-09-09 21:06:42 +0200

    conection ignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,20 @@
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddDbContext<MyDbContext>(conf=>
+{
+    conf.UseNpgsql(Environment.GetEnvironmentVariable("MyConnectionString"));
+});
+
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{
+    var objects = dbContext.Todos.ToList(); // the way to take all the rows in the todotable. It knows abot todo entity
+});
 app.Run();
